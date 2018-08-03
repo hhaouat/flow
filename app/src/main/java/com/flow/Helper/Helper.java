8249903dd2f4bcdfb690b8326f309dd2c4e35664
Sajad Jaward <sajad.jaward1@gmail.com>
2018-08-03 06:31:48 +0530

    created this new file

---
+++ app/src/main/java/com/flow/Helper/Helper.java
@@ -0,0 +1,18 @@
+package com.flow.Helper;
+
+/**
+ * Created by Administrator on 8/3/2018.
+ */
+
+public class Helper {
+    public enum cityNames{
+        makkah,
+        madinah,
+        mina,
+        arafat,
+        muzdalifah,
+        minaCamps,
+        jamratstoning,
+        slaugterHouse
+    }
+}
